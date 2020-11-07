adb53e1b0a981d5ebfcd65072fbd1773067346d3
MohFahmi27 <mohammadfahmi417@gmail.com>
2020-11-07 13:41:47 +0800

    initial commit

---
+++ assets/js/slider.js
@@ -0,0 +1,128 @@
+$(".slider-nav").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    focusOnSelect: true,
+  });
+
+$(".slider-nav-product").slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots: false,
+  focusOnSelect: false,
+  autoplay: false,
+  centerMode:true,
+  responsive: [
+    {
+      breakpoint: 1124,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerMode: false
+      }
+    },
+    {
+      breakpoint: 850,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,        
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true
+      }
+    },
+    {
+      breakpoint: 380,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$("a").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+  
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  } // End if
+});
+
+$(".slider-nav-product").slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots: false,
+  focusOnSelect: false,
+  autoplay: false,
+  centerMode:true,
+  responsive: [
+    {
+      breakpoint: 1124,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerMode: false
+      }
+    },
+    {
+      breakpoint: 850,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,        
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true
+      }
+    },
+    {
+      breakpoint: 380,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
